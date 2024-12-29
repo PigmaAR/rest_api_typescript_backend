64c83f8f4f9753c03143b278db05c5e2e6f1d158
RamunnoAJ <ramunnoagustin@gmail.com>
2024-12-29 20:26:35 -0300

    feat: tests

--- src/handlers/__tests__/product.spec.ts
+++ src/handlers/__tests__/product.spec.ts
@@ -2,16 +2,64 @@ import request from "supertest";
 import server from "../../server";
 
 describe("POST /api/products", () => {
+  it("should display validation errors", async () => {
+    const response = await request(server).post("/api/products").send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(3);
+  });
+
+  it("should validate that the price is greater than 0", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "Monitor Curvo",
+      price: 0,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+  });
+
+  it("should validate that the price is valid", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "Monitor Curvo",
+      price: "hola",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+  });
+
   it("should create a new product", async () => {
     const response = await request(server)
       .post("/api/products")
       .send({ name: "Mouse - Testing", price: 300 });
 
     expect(response.status).toEqual(201);
-    expect(response.status).toHaveProperty("data");
+    expect(response.body).toHaveProperty("data");
 
     expect(response.status).not.toBe(404);
     expect(response.status).not.toBe(200);
     expect(response.status).not.toHaveProperty("error");
+  });
+});
+
+describe("GET /api/products", () => {
+  it("should check if api/products url exists", async () => {
+    const response = await request(server).get("/api/products");
+    expect(response.status).not.toBe(404);
+  });
+
+  it("GET a JSON with products", async () => {
+    const response = await request(server).get("/api/products");
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.data).toHaveLength(1);
+
+    expect(response.status).not.toBe(404);
+    expect(response.body).not.toHaveProperty("errors");
   });
 });
