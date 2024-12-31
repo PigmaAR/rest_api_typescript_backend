ac5ac6cf138bef00317677f8de4d340922eed70b
RamunnoAJ <ramunnoagustin@gmail.com>
2024-12-31 20:14:35 -0300

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./router";
 import db from "./config/db";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
 
@@ -23,7 +25,19 @@ connectDB();
 
 const server = express();
 
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"), false);
+    }
+  },
+};
+
+server.use(morgan("dev"));
 server.use(express.json());
+server.use(cors(corsOptions));
 server.use("/api/products", router);
 
 server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
