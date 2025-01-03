71844e3f118fc22053c05032ed3849f4a59898e9
RamunnoAJ <ramunnoagustin@gmail.com>
2025-01-03 11:01:41 -0300

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ const corsOptions: CorsOptions = {
     if (origin === process.env.FRONTEND_URL) {
       callback(null, true);
     } else {
-      callback(new Error("Error de CORS"), false);
+      callback(null, true);
     }
   },
 };
